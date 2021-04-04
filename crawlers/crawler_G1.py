4e0dbc46e0bfe9c6891fe42289e47a47b2262629
Bruno Tatsuya <brunomasunaga@gmail.com>
2021-04-03 22:57:22 -0300

    Finished G1 crawler

--- crawlers/crawler_G1.py
+++ crawlers/crawler_G1.py
@@ -1,22 +1,23 @@
 """Author: Bruno Tatsuya Masunaga Santos
 Organization: Universidade Federal do ABC (UFABC)
-Project: PGC-UFABC
+Project: COVID-19 Fake News Detection
 Created in: 2021-04-03
 Description: Crawler to get news from G1
 """
 
 from bs4 import BeautifulSoup
-from datetime import datetime
+from datetime import datetime, timedelta
 from pymongo import MongoClient
 from requests import get
 from urllib.parse import unquote
+from crawlers_common import NestedLoopBreaker
 from crawlers_common import get_between
 from crawlers_common import mongo_collection
+from crawlers_common import insert_raw_collection
 
 ENDPOINT = 'https://g1.globo.com/busca/'
 ORDER = 'recent'
 SPECIES = 'notícias'
-G1_COLLECTION = mongo_collection('raw_G1')
 
 def get_html_news_G1(subject, date_from, time_from, date_to, time_to, page):
     """Sends GET HTTP request in order to get the HTML of G1 news query page
@@ -35,14 +36,16 @@ def get_html_news_G1(subject, date_from, time_from, date_to, time_to, page):
 
     uri = ENDPOINT                                          # set endpoint
     uri += '?q=' + subject                                  # set query subject
-    uri += '&page' + str(page)                              # set query page
-    uri += '&order' + ORDER                                 # set sorting rule
-    uri += '&species' + SPECIES                             # set query content type
-    uri += '&from' + date_from + 'T' + time_from + '-0300'  # set query init time range
-    uri += '&to' + date_to + 'T' + time_to + '-0300'        # set query end time range
+    uri += '&page=' + str(page)                              # set query page
+    uri += '&order=' + ORDER                                 # set sorting rule
+    uri += '&species=' + SPECIES                             # set query content type
+    uri += '&from=' + date_from + 'T' + time_from + '-0300'  # set query init time range
+    uri += '&to=' + date_to + 'T' + time_to + '-0300'        # set query end time range
 
     try:
+        print('Getting URI: ' + uri + '\n')
         response = get(uri)
+        print('Result GET: ' + str(response) + '\n')
         return response.text
     except:
         return 'Failed to get HTML'
@@ -59,10 +62,12 @@ def scrap_news_G1(html):
 
     news = []
     if not (html == 'Failed to get HTML'):
-        page = BeautifulSoup(html)
+        page = BeautifulSoup(html, features='html.parser')
         news_containers = page.find_all('div', {'class': 'widget--info__text-container'})
         for nc in news_containers:
             # Get and treat news title
+            if 'widget--info__title--ad' in str(nc):
+                continue
             title = nc.find('div', {'class': 'widget--info__title product-color'}).text
             title = title.rstrip('\n').strip()
             # Get and treat news link
@@ -72,9 +77,12 @@ def scrap_news_G1(html):
             # Get and treat news date
             date_string = nc.find('div', {'class': 'widget--info__meta'}).text
             if "há" in date_string:
-                response = get(link)
-                page_news = BeautifulSoup(response.text)
-                date_string = page_news.find('time', {'itemprop': 'datePublished'}).text.strip()
+                int_value = int(get_between(date_string, ' ', ' '))
+                if "hora" in date_string:
+                    date = datetime.now() - timedelta(hours=int_value)
+                if "dia" in date_string:
+                    date = datetime.now() - timedelta(days=int_value)
+            else:
                 date = datetime.strptime(date_string, '%d/%m/%Y %Hh%M')
             # Construct object
             news_data = {'title': title, 'link': link, 'datetime': date}
@@ -82,18 +90,43 @@ def scrap_news_G1(html):
 
     return news
 
-def insert_raw_G1(data):
-    """Inserts one object into raw_G1 MongoDB collection
-  
-    Args:
-        data (dictionary): Data for inserting into raw_G1 collection
-        (format: {'title': string, 'link': string, 'datetime': datetime})
+if __name__ == '__main__':
+    # Set target collection
+    G1_COLLECTION = mongo_collection('raw_G1')
 
-    Returns:
-        string: ID of inserted object
-    """
-    try: 
-        inserted_id = G1_COLLECTION.insert_one(data).inserted_id
-        return inserted_id
+    # Get max datetime in collection (for duplicity control)
+    try:
+        MAX_DATETIME = G1_COLLECTION.find().sort([("datetime", -1)]).limit(1)[0]['datetime']
     except:
-        return ''
+        MAX_DATETIME = datetime(1900,1,1,0,0,0)
+
+    # Initializing datetime for querying 
+    start_datetime = datetime.now()
+    query_date = datetime(start_datetime.year, start_datetime.month, start_datetime.day)
+    query_date_str = query_date.strftime("%Y-%m-%d")
+
+    # Processing logic
+    try:
+        while (True):
+            for page in range(1,41):
+                html = get_html_news_G1('coronavirus', query_date_str, '00:00:00', 
+                                        query_date_str, '23:59:59', page)
+                news_scrapped = scrap_news_G1(html)
+                if not news_scrapped:
+                    break
+                for news in news_scrapped:
+                    # If news datetime is older than database registries, stop
+                    if news['datetime'] < MAX_DATETIME:
+                        raise NestedLoopBreaker()
+                    inserted_id = insert_raw_collection(news, G1_COLLECTION)
+                    print('Inserted object ' + inserted_id + ', dated on ' + 
+                    news['datetime'].strftime("%Y-%m-%d") + 
+                    'into raw_G1 collection')
+            # Previous day
+            query_date = query_date - timedelta(days=1)
+            query_date_str = query_date.strftime("%Y-%m-%d")
+
+    except NestedLoopBreaker:
+        pass
+    
+    print('Execution finished')
--- crawlers/crawlers_common.py
+++ crawlers/crawlers_common.py
@@ -1,12 +1,15 @@
 """Author: Bruno Tatsuya Masunaga Santos
 Organization: Universidade Federal do ABC (UFABC)
-Project: PGC-UFABC
+Project: COVID-19 Fake News Detection
 Created in: 2021-04-03
 Description: Common functions to help crawlers development
 """
 
 import re
 from pymongo import MongoClient
+
+
+class NestedLoopBreaker(Exception): pass
 
 def get_between(base_string, before, after, includes = False):
     """Gets matched substring between substrings
@@ -24,7 +27,7 @@ def get_between(base_string, before, after, includes = False):
     matched = re.search(before + '(.*)' + after, base_string)
     if (includes):
         return matched[0]
-    return [1]
+    return matched[1]
 
 def mongo_collection(collection):
     """Creates new object to a specific MongoDB collection
@@ -36,6 +39,23 @@ def mongo_collection(collection):
         pymongo.collection.Collection: MongoDB collection targered
     """
     client = MongoClient('localhost', 27017)
-    db = client['pgc_ufabc']
+    db = client['covid-fake-news-detection']
     collection = db[collection]
     return collection
+
+
+def insert_raw_collection(data, collection):
+    """Inserts one object into MongoDB collection
+  
+    Args:
+        data (dictionary): Data for inserting into collection
+        collection (pymongo.collection.Collection): MongoDB collection targered
+
+    Returns:
+        string: ID of inserted object
+    """
+    try: 
+        inserted_id = collection.insert_one(data).inserted_id
+        return str(inserted_id)
+    except:
+        return ''
